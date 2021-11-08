52aa51259663970d0fb6b98c738efdc5fb5a1ef7
mrkbnv <93657453+mrkbnv@users.noreply.github.com>
2021-11-08 23:52:43 +0700

    Initial commit

---
+++ src/MyApp.js
@@ -0,0 +1,21 @@
+function MyApp() {
+  return (
+    <div>
+      <h1>Marko</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur.
+      </p>
+      <ul>
+        <li>Maldives</li>
+        <li>Raja Ampat</li>
+        <li>Paris</li>
+      </ul>
+    </div>
+  );
+}
+
+export default MyApp;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import MyApp from "./MyApp.js";
+ReactDOM.render(<MyApp />, document.getElementById("root"));
